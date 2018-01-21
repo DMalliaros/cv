7992a51b6c812299cb444a3f88d9b01bb07eabc9
D.Malliaros <dimi.malliaros@gmail.com>
2018-01-21 21:06:34 +0200

    init project

---
+++ src/js/index.js
@@ -0,0 +1,60 @@
+
+require("styles/style.less");
+require("bootstrap");
+require("modules/jqcloud2/dist/jqcloud.js");
+
+import skills from "../json/skills.json";
+
+import $  from 'jquery';
+import _ from 'lodash';
+
+
+
+$(document).ready(function() {
+
+    let mapOfSkills = {} ;
+    mapOfSkills["all"] = []
+    _.each(skills,function (skill) {
+        mapOfSkills[skill.title] = []
+        _.each(skill.data,function (data) {
+            let sk = {
+                text : data[1]
+                , weight : data[0]
+            };
+            mapOfSkills["all"].push(sk);
+            mapOfSkills[skill.title].push(sk);
+
+        });
+    });
+    const $modal = $("#myModal");
+
+    initModal($modal,mapOfSkills)
+
+
+});
+
+
+function initModal($modal, mapOfSkills) {
+    const $cloude = $("[data-cloud]") ;
+
+    let cloudejQCloud;
+
+    $modal.on('shown.bs.modal', function (event) {
+
+        let button = $(event.relatedTarget)
+            , data =  button.data('skils');
+
+        cloudejQCloud = $cloude.jQCloud(mapOfSkills[data],{
+            width: 720 ,
+            height: 400
+        });
+
+    });
+
+
+    $modal.on('hide.bs.modal', function (event) {
+        if (typeof cloudejQCloud === "object"){
+            cloudejQCloud.jQCloud("destroy");
+        }
+    });
+}
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const resolve = path.resolve;
+const webpack = require('webpack');
+const NunjucksWebpackPlugin = require( "nunjucks-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+const jsonContext = require('./src/json/jsonContext.json');
+const jsonSkills = require('./src/json/skills.json');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const _ =  require('lodash');
+
+const extractLess =  new ExtractTextPlugin({ // Extracrs the compiled styles from their bundled js module to a .css file
+    filename: 'styles/styles.css',
+    disable: false,
+    allChunks: true
+});
+
+const extractNunjucks = new NunjucksWebpackPlugin({
+
+    templates: [
+        {
+            from: path.join(__dirname, 'src/templates', 'index.njk')
+            ,to: path.resolve(__dirname, 'dist','index.html')
+            , context: _.extend(jsonContext, {skils:jsonSkills})
+            , writeToFileEmit: true
+        }
+    ]
+    , configure: (path.join(__dirname, 'src/templates'),{watch: true})
+});
+
+module.exports = env => {
+    return {
+        context: resolve('')
+        , entry: {
+            index: './src/js/index.js'
+        }
+        , output: {
+            filename: 'scripts/[name].bundle.js',
+            path: path.join(__dirname, 'dist')
+        }
+        , resolve: {
+            alias: {
+                'jquery': path.join(__dirname, 'node_modules/jquery/dist/jquery')
+                , 'styles': path.join(__dirname, 'src/less')
+                , 'modules': path.join(__dirname, 'node_modules')
+            }
+        }
+        , devtool: env.prod ? 'source-map' : 'eval'
+        , devServer: {
+            contentBase: path.join(__dirname, "dist"),
+            compress: true,
+            port: 9000
+        }
+        , module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    },
+                    exclude: /(node_modules)/
+                }
+                , {
+                    test: /\.less$/,
+                    loader: ExtractTextPlugin.extract({
+                        fallback: "style-loader",
+                        use: ['css-loader', 'less-loader']
+                    }),
+                    exclude: /(node_modules|bower_components)/
+                }
+                , {
+                    test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)|\.png($|\?)/,
+                    loader: 'file-loader?publicPath=../&name=./fonts/[hash].[ext]'
+                }
+
+            ]
+        }
+        , plugins: [
+            new webpack.ProvidePlugin({
+                $: "jquery"
+                , jQuery: "jquery"
+            })
+            , extractLess
+            , extractNunjucks
+            , new CopyWebpackPlugin([
+                {from: 'src/img/favicon.ico', to: './'}
+                , {from: 'src/img/', to: 'img'}
+            ])
+        ]
+    }
+};
